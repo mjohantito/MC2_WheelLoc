80ff859d47519e8e5d347ae1b6c75dd54eb6e806
Manuel Johan Tito <mjohantito@student.ciputra.ac.id>
2023-06-16 15:54:48 +0700

    try2

--- MC2_WheelHelp/ContentView.swift
+++ MC2_WheelHelp/ContentView.swift
@@ -38,7 +38,7 @@ struct ContentView: View {
                     }
                 }
             }
-            Text("Select an item, markicoblagi")
+            Text("Select an item, markicobbb")
         }
     }
 
