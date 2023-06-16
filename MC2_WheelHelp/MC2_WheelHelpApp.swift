f4c6a62c07e1dc3df1a608c90741f839fa9fc161
Manuel Johan Tito <mjohantito@student.ciputra.ac.id>
2023-06-16 15:30:18 +0700

    Initial Commit

---
+++ MC2_WheelHelp/MC2_WheelHelpApp.swift
@@ -0,0 +1,20 @@
+//
+//  MC2_WheelHelpApp.swift
+//  MC2_WheelHelp
+//
+//  Created by Manuel Johan Tito on 16/06/23.
+//
+
+import SwiftUI
+
+@main
+struct MC2_WheelHelpApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
