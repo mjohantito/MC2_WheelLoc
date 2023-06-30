05f01c473803674ce35921f6a41d66ce9b892890
dyahputrin <putri.nariswari@gmail.com>
2023-06-30 14:54:12 +0700

    add function to insert health facilities to cloudkit

--- MC2_WheelHelp/CloudKit.swift
+++ MC2_WheelHelp/CloudKit.swift
@@ -37,6 +37,33 @@ func addRecordToCloudKit(name:String, address:String, category:String, health_fa
     }
 }
 
+func addRecordHealthFacilitiesToCloudKit(name:String, address:String, category:String, latitude:Double, longitude:Double, img_prefix:[String], img_suffix:[String], fsq_id:String) {
+    let container = CKContainer(identifier: "iCloud.com.ada.MC2-WheelHelp-Putri")
+    let recordType = "HealthFacilities"
+    let record = CKRecord(recordType: recordType)
+
+    record["fsq_id"] = fsq_id as CKRecordValue
+    record["name"] = name as CKRecordValue
+    record["address"] = address as CKRecordValue
+    record["category"] = category as CKRecordValue
+    record["latitude"] = latitude as CKRecordValue
+    record["longitude"] = longitude as CKRecordValue
+    record["img_prefix"] = img_prefix as CKRecordValue
+    record["img_suffix"] = img_suffix as CKRecordValue
+
+    let database = container.publicCloudDatabase
+
+    database.save(record) { (savedRecord, error) in
+        if let error = error {
+            // Handle the error
+            print("Error saving record: \(error.localizedDescription)")
+        } else {
+            // Handle the success
+            print("Record saved successfully")
+        }
+    }
+}
+
 
 
 
