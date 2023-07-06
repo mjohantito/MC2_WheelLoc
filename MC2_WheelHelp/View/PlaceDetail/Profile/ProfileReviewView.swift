3030c554bd73c5456776dfe4683c6d1c309ca2c9
Manuel Johan Tito <mjohantito@student.ciputra.ac.id>
2023-07-06 14:56:43 +0700

    grouping profile & add profileReviewView

---
+++ MC2_WheelHelp/View/PlaceDetail/Profile/ProfileReviewView.swift
@@ -0,0 +1,28 @@
+//
+//  ProfileReviewView.swift
+//  MC2_WheelHelp
+//
+//  Created by Manuel Johan Tito on 06/07/23.
+//
+
+import SwiftUI
+
+struct ProfileReviewView: View {
+    var body: some View {
+        NavigationStack{
+            ScrollView{
+                ForEach (0..<3, id: \.self){_ in
+                    ReviewCardView(userNameReview: "Angelo Kusuma", dateReview: "30 Mar 2023", ratingReview: 4.5, titleReview: "Bagus Banget!", descriptionReview: "Disini fasilitas buat pengguna kursi roda aman banget, bahkan toiletnya disediain khusus buat disabilitas!")
+                }
+
+                Spacer()
+            }.padding(.top,16)
+        } .navigationTitle("Ulasan")
+    }
+}
+
+struct ProfileReviewView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileReviewView()
+    }
+}
